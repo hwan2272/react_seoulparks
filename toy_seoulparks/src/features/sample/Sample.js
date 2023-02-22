db115ef9cabe555733b98772be9bf74afb0689b4
hwan2272 <hwan2230@naver.com>
2023-02-22 20:09:13 +0900

    init

---
+++ toy_seoulparks/src/features/sample/Sample.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import * as sampleJs from './js/sample';
+
+export function Sample() {
+
+    useEffect(() => {
+        // const body = document.body;
+        // let script = document.createElement("script");
+        // script.src = 'https://cdnjs.cloudflare.com/ajax/libs/redux/3.6.0/redux.js';
+        // script.async = true;
+        // body.appendChild(script);
+    })
+}
---
+++ toy_seoulparks/src/features/sample/js/sample.js
@@ -0,0 +1,50 @@
+import { legacy_createStore as createStore } from "@reduxjs/toolkit";
+
+const elNumber = document.getElementById('number');
+const btnIncrement = document.getElementById('increment');
+const btnDecrement = document.getElementById('decrement');
+
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+
+const increment = (diff) => ({ type: INCREMENT, diff:diff });
+const decrement = () => ({ type: DECREMENT });
+
+const initialState = {
+  number : 0
+}
+
+
+const counter = (state = initialState, action) => {
+  console.log(action);
+  switch(action.type) {
+    case INCREMENT :
+      return {
+        number: state.number + action.diff 
+      };
+    case DECREMENT : 
+      return {
+        number : state.number - 1
+      };
+    default : 
+      return state;
+  }
+}
+
+//const {createStore} = Redux;
+const store = createStore(counter);
+
+const render = () => {
+  elNumber.innerText = store.getState().number;
+}
+
+store.subscribe(render);
+render();
+
+btnIncrement.addEventListener('click', () => {
+  store.dispatch(increment(25));
+})
+
+btnDecrement.addEventListener('click', () => {
+  store.dispatch(decrement());
+})
