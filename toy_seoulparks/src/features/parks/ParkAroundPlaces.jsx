4dce73dd23598e78851cfcd85608cba5250b89b7
hwan2272 <hwan2230@naver.com>
2023-03-10 17:45:20 +0900

    * 설힐공 작업 - ChatGpt 기능 이용 테스트 - 코드리뷰기능

--- toy_seoulparks/src/features/parks/ParkAroundPlaces.jsx
+++ toy_seoulparks/src/features/parks/ParkAroundPlaces.jsx
@@ -10,6 +10,61 @@ export function ParkAroundPlaces({lat, lng}) {
   const ps = new kakao.maps.services.Places(); 
   const userAgent = window.navigator.userAgent.toLowerCase();
 
+  /*
+// useEffect를 사용하여 컴포넌트가 마운트될 때와 lat, lng이 변경될 때마다 실행되도록 함
+useEffect(() => {
+  // kakao.maps.load()를 사용하여 카카오 맵 API를 로드함
+  kakao.maps.load(() => {
+    // lat과 lng이 정의되어 있을 때만 지도를 초기화하도록 함
+    if(lat != undefined && lng != undefined) {
+      // kakaoInitMap() 함수를 사용하여 지도를 초기화함
+      kakaoInitMap(lat, lng);
+      // fetchPlaceFD6Data() 함수를 사용하여 FD6 데이터를 가져옴
+      const fetchPlaceFD6Data = async () => {
+        // userAgent에 'kakao'가 포함되어 있을 때만 실행되도록 함
+        if(userAgent.includes('kakao')) {
+          // userAgent에 'iPhone' 또는 'iPad'가 포함되어 있을 때만 실행되도록 함
+          if(navigator.userAgent.match(/iPhone|iPad/i)) {
+            // IOS kakao인앱브라우저에서는 제대로 동작하지 않을 수 있으므로 경고 메시지를 띄움
+            //setPlaceFD6List([]); // 주석 처리된 코드는 불필요한 코드이므로 제거해도 됨
+          }
+        }
+        else {
+          // getPlaceFD6Data() 함수를 사용하여 FD6 데이터를 가져옴
+          setPlaceFD6List(await getPlaceFD6Data(lat,lng));
+        }
+      }
+      // fetchPlaceCE7Data() 함수를 사용하여 CE7 데이터를 가져옴
+      const fetchPlaceCE7Data = async () => {
+        // userAgent에 'kakao'가 포함되어 있을 때만 실행되도록 함
+        if(userAgent.includes('kakao')) {
+          // userAgent에 'iPhone' 또는 'iPad'가 포함되어 있을 때만 실행되도록 함
+          if(navigator.userAgent.match(/iPhone|iPad/i)) {
+            // IOS kakao인앱브라우저에서는 제대로 동작하지 않을 수 있으므로 경고 메시지를 띄움
+            //setPlaceFD6List([]); // 주석 처리된 코드는 불필요한 코드이므로 제거해도 됨
+          }
+        }
+        // getPlaceCE7Data() 함수를 사용하여 CE7 데이터를 가져옴
+        setPlaceCE7List(await getPlaceCE7Data(lat,lng));
+      }
+      // fetchPlaceFD6Data()와 fetchPlaceCE7Data() 함수를 실행함
+      fetchPlaceFD6Data();
+      fetchPlaceCE7Data();
+    }
+  });
+},[lat, lng]);
+
+좋은 점:
+- useEffect를 사용하여 컴포넌트가 마운트될 때와 lat, lng이 변경될 때마다 실행되도록 함
+- 카카오 맵 API를 로드하는 kakao.maps.load() 함수를 사용함
+- lat과 lng이 정의되어 있을 때만 지도를 초기화하도록 함
+- async/await를 사용하여 비동기적으로 데이터를 가져옴
+
+나쁜 점:
+- 주석 처리된 코드는 불필요한 코드이므로 제거해야 함
+- 경고 메시지를 띄우는 것보다는, 사용자가 이해할 수 있는 방법으로 문제를 해결하는 것이 좋음
+*/
+
   useEffect(() => {
     kakao.maps.load(() => {
       if(lat != undefined && lng != undefined) {
